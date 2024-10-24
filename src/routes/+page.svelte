b920e743df0d29e2a16e69d30ee57cd2093732e4
Louis Van Aken <vanakenlouis@gmail.com>
2024-10-24 16:33:04 +0200

    Remove homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,51 @@
+<script lang=ts>
+	import { onMount } from 'svelte';
+	import { HandRenderer } from './core/HandRenderer.svelte';
+	import Drawing from './ui/Drawing.svelte';
+	import Interface from './ui/Interface.svelte';
+
+	let handContainer: HTMLElement | undefined = $state();
+	let renderer: HandRenderer;
+
+	onMount(async () => {
+		if (!handContainer) return;
+		renderer = new HandRenderer(handContainer);
+		await renderer.init();
+		renderer.start();
+	})
+</script>
+
 <svelte:head>
 	<title>Virtual Doodle</title>
-	<meta name="description" content="Play Virtual Doodle Online" />
+	<meta name="description" content="Virtual Doodle Game" />
 </svelte:head>
 
-<h1>Virtual Doodle</h1>
-<p><a href="/play">Start a game</a></p>
+<div class="container">
+	<div bind:this={handContainer} class="hand"></div>
+	<div class="canvas">
+		<Drawing />
+	</div>
+	<Interface />
+</div>
+
+<style>
+	.container {
+		position: relative;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100vw;
+		height: 100vh;
+	}
+	.hand {
+		position: absolute;
+		z-index: 20;
+	}
+	.canvas {
+		height: 100%;
+		width: 100%;
+		background-size: 1rem 1rem;
+  	background-image: radial-gradient(circle, rgba(var(--ui-bg), 0.1) 1px, rgba(0, 0, 0, 0) 1px);
+		background-position: center;
+	}
+</style>
